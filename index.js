83dd783d9c2ae5475537e1128a596470468408da
inst760fall2024 <inst760fall2024@gmail.com>
2024-10-22 14:14:57 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ import {
 } from 'https://public.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js';
 
 var viz = new TableauViz();
-viz.src = "https://public.tableau.com/views/CarSample/Dashboard1";
+viz.src = "https://public.tableau.com/views/Class1_17296199644600/Dashboard1";
 $('#tableauViz').append(viz);
